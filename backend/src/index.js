b3aef6c6796f3b0021081d1e5eb6bb5eeeeac352
Aaditya Gautam <codeuntillive@gmail.com>
2026-02-02 15:55:11 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,9 +42,10 @@ app.use("/api/message",checkauth, messageRouter);
 // reday for deployment
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname,'../frontend/dist')));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../frontend/dist/index.html'));
-  })
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+});
+
 }
 
 
